d41cd8e8a06d2e2a1ba84551cb5d6e65f1edb71d
Shardian <32742401+Shardian@users.noreply.github.com>
2017-10-18 18:54:41 +0300

    Revert "Card layout + Navbar"
    This reverts commit ebbc57ba66288fc4acd2da13da4b8385cdca1af8.

--- js/lightning.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 140) {
-      $(".sticky-event").addClass('navbar-sticked');
-    } else {
-      $(".sticky-event").removeClass('navbar-sticked');
-    }
-  });
-});
