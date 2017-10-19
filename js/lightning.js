77eeca4f405a011113db5697cd32773ca914fee1
Andrew <andrew.golovenko@gmail.com>
2017-10-20 00:15:36 +0300

    Added fucking selection system

--- js/lightning.js
+++ js/lightning.js
@@ -7,23 +7,63 @@ $(document).ready(function() {
     }
   });*/
 
-  let cart = [];
+  let iPhone5 = [];
+  let iPhone5s = [];
+  let iPhoneSE = [];
+  let iPhone6 = [];
+  let iPhone6s = [];
+  let iPhone7 = [];
   let price = 0;
 
+  let cart = new Map();
+  cart.set("5", iPhone5);
+  cart.set("5s", iPhone5s);
+  cart.set("SE", iPhoneSE);
+  cart.set("6", iPhone6);
+  cart.set("6s", iPhone6s);
+  cart.set("7", iPhone7);
+
+
   const redraw = function() {
-    let size = cart.length;
     let html = "";
-    while(size > 0) {
-      html += "<tr data-index=\"" + (size - 1) + "\">" + $(cart[size - 1]).html() + "</tr>";
-      size--;
+
+    for (let [phone, item] of cart) {
+      let size = item.length;
+      if (size > 0) {
+        html += '<tr class="table-info"><td colspan="2">iPhone ' + phone + '</td></tr>';
+      }
+      while(size > 0) {
+        html += "<tr data-phone=\"" + phone + "\" data-index=\"" + (size - 1) + "\">" + $(item[size - 1]).html() + "</tr>";
+        size--;
+      }
     }
-    console.log(html);
     $("#cart-list").html(html);
   };
 
   $(".service-table tr").click(function() {
     if ($(this).hasClass('bg-primary')) {
       $(this).removeClass('bg-primary');
+      let cart;
+      switch ($(this).parent().attr('id')) {
+        case "iPhone5-tbody":
+          cart = iPhone5;
+          break;
+        case "iPhone5s-tbody":
+          cart = iPhone5s;
+          break;
+        case "iPhoneSE-tbody":
+          cart = iPhoneSE;
+          break;
+        case "iPhone6-tbody":
+          cart = iPhone6;
+          break;
+        case "iPhone6s-tbody":
+          cart = iPhone6s;
+          break;
+        case "iPhone7-tbody":
+          cart = iPhone7;
+          break;
+      }
       let index = cart.indexOf(this);
       if (index >= 0) {
         cart.splice(index, 1);
@@ -38,7 +78,28 @@ $(document).ready(function() {
       redraw();
     } else {
       $(this).addClass('bg-primary');
-      cart.push(this);
+
+      switch ($(this).parent().attr('id')) {
+        case "iPhone5-tbody":
+          iPhone5.push(this);
+          break;
+        case "iPhone5s-tbody":
+          iPhone5s.push(this);
+          break;
+        case "iPhoneSE-tbody":
+          iPhoneSE.push(this);
+          break;
+        case "iPhone6-tbody":
+          iPhone6.push(this);
+          break;
+        case "iPhone6s-tbody":
+          iPhone6s.push(this);
+          break;
+        case "iPhone7-tbody":
+          iPhone7.push(this);
+          break;
+      }
+
       price += parseInt($(this).children('td').eq(1).html().substr(1));
       if (price > 0) {
         if ($(".price").hasClass('d-none')) {
