89a7ac94f40efe54db65b5175d45cbf8b1fc696e
Andrew <andrew.golovenko@gmail.com>
2017-10-19 16:01:57 +0300

    Removed scroll script

--- js/lightning.js
+++ js/lightning.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  $(window).scroll(function() {
+  /*$(window).scroll(function() {
     if ($(this).scrollTop() > 140) {
       $(".sticky-event").addClass('navbar-sticked');
     } else {
       $(".sticky-event").removeClass('navbar-sticked');
     }
-  });
+  });*/
 });
