91352575bab3fbbd87282171be68054d6ee43aa6
Andrew <andrew.golovenko@gmail.com>
2017-10-19 22:39:10 +0300

    Супер охуенный сервис

--- js/lightning.js
+++ js/lightning.js
@@ -7,11 +7,32 @@ $(document).ready(function() {
     }
   });*/
 
+  let cart = [];
+  let price = 0;
+
   $(".service-table tr").click(function() {
     if ($(this).hasClass('bg-primary')) {
       $(this).removeClass('bg-primary');
+      let index = cart.indexOf(this);
+      if (index >= 0) {
+        cart.splice(index, 1);
+        price -= parseInt($(this).children('td').eq(1).html().substr(1));
+      }
+      if (price <= 0) {
+        $(".price").addClass('d-none');
+      } else {
+        $(".price").html("Общая стоимость: ₴" + price);
+      }
     } else {
       $(this).addClass('bg-primary');
+      cart.push(this);
+      price += parseInt($(this).children('td').eq(1).html().substr(1));
+      if (price > 0) {
+        if ($(".price").hasClass('d-none')) {
+          $(".price").removeClass('d-none');
+        }
+        $(".price").html("Общая стоимость: ₴" + price);
+      }
     }
   });
 
