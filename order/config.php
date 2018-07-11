0974270f4ed824186a49b314f05c8713a0b8d35e
Andrew <andrikgolova@gmail.com>
2018-07-11 19:54:43 +0300

    ))

--- order/config.php
+++ order/config.php
@@ -2,7 +2,7 @@
 
   define('DB_NAME', 'lightning');
   define('DB_USER', 'root');
-  define('DB_PASSWORD', '19FylhbR99');
+  define('DB_PASSWORD', '');
   define('DB_HOST', '127.0.0.1');
   define('DB_TIMEZONE', 'Europe/Kiev');
 
